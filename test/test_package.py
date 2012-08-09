d143b74f18419d79b2a8d1150e3263cb8daa08a5
Mike Stewart <mike@mikestewart.net>
2012-08-08 22:20:47 -0700

    Add tests for deb packaging.
    Plus fix test brokenness due to multiple test cases
    with identical names.

--- test/test_package.py
+++ test/test_package.py
@@ -12,7 +12,14 @@ class PackageDelegationTest(TestCase):
         rpm.install.assert_called_once_with('mypkg')
 
     @mock.patch('patchwork.packages.distro_family')
-    def test_redhat_delegation(self, distro_family):
+    @mock.patch('patchwork.packages.deb')
+    def test_debian_delegation(self, deb, distro_family):
+        distro_family.return_value = 'debian'
+        packages.install('mypkg')
+        deb.install.assert_called_once_with('mypkg')
+
+    @mock.patch('patchwork.packages.distro_family')
+    def test_unknown_delegation(self, distro_family):
         distro_family.return_value = 'other'
         with self.assertRaises(NotImplementedError):
             packages.install('mypkg')
@@ -31,3 +38,17 @@ class PackageQueryTest(TestCase):
     def test_redhat_multiple_query(self, run):
         packages.rpm.is_installed('mypkg1', 'mypkg2')
         run.assert_called_once_with('rpm --query --quiet mypkg1 mypkg2')
+
+    @mock.patch('patchwork.packages.deb.sudo')
+    @mock.patch('patchwork.packages.deb.run')
+    def test_debian_unary_query(self, run, sudo):
+        for answer in (True, False):
+            run.reset_mock()
+            run.return_value.succeeded = answer
+            self.assertEqual(answer, packages.deb.is_installed('mypkg'))
+            run.assert_called_once_with('dpkg-query --show mypkg')
+
+    @mock.patch('patchwork.packages.deb.run')
+    def test_debian_multiple_query(self, run):
+        packages.deb.is_installed('mypkg1', 'mypkg2')
+        run.assert_called_once_with('dpkg-query --show mypkg1 mypkg2')
