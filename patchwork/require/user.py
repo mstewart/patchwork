2691537325b4bb4ba454727cf1ca3c3e2d237a35
Mike Stewart <mike@mikestewart.net>
2012-08-08 23:46:50 -0700

    Add require.user, adapted from fabtools

---
+++ patchwork/require/user.py
@@ -0,0 +1,33 @@
+from fabric.api import sudo, run
+from fabric.contrib.files import exists
+from ..user import *
+
+import . as require
+
+def user(name, home=None, groups=None):
+    """
+    Require a user to exist, having each of the specified set of
+    groups as its supplemental groups.
+    The user's primary gid will be a new user-specific group.
+
+    If the user already exists, ensure it has *at least* the specified
+    set of supplemental groups.  Extra group memberships will not be removed.
+
+    If the user already exists, ensure it has the specified homedir.
+    Error out if this is not the case (as changing the homedir of a
+    possibly in-use user is pretty dangerous, won't update environment
+    variables for running processes, will be refused on Linux systems, etc.
+    It's also a pretty strong sign that the system is in an unexpected
+    state and requires manual attention.)
+    """
+    if not exists(name):
+        create(name, home=home)
+    elif home:
+        observed_home = get_homedir(name)
+        if not observed_home == home:
+            raise RuntimeError("User %s already exists with homedir %s (expected %s)" %
+                    (name, observed_home, home))
+        require.directory(home, owner=name, runner=sudo)
+    if groups:
+        for group in groups:
+            add_to_group(name, group)
