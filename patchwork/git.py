4867afd7d76b45ac6b5e9acf0254cfdd2b0af375
Mike Stewart <mike@mikestewart.net>
2012-08-10 19:42:39 -0700

    Fix typo in git.py and add git.repo() function

--- patchwork/git.py
+++ patchwork/git.py
@@ -17,30 +17,36 @@ def is_git_repo(repo_path, runner=None):
     with settings(hide('everything'), warn_only=True):
         with cd(repo_path):
             show_toplevel = runner('git rev-parse --show-toplevel')
-    return show_toplevel.succeeded and show_toplevel.stdout == repo_path:
+    return show_toplevel.succeeded and show_toplevel.stdout == repo_path
 
 
 def clone(source_url, destination_repo_path=None, runner=None):
     """
-    Clone a git repo from the given URL to destination_repo_path.
-
-    If ``destination_repo_path`` is not specified, a repository will be
-    created under the working directory (as is the default for ``git clone``).
+    Clone a git repo from the ``source_url`` to ``destination_repo_path``.
     """
     if runner is None:
         runner = run
-    cmd = "git clone %s" % source_url
-    if destination_repo_path:
-        cmd += " %s" % destination_repo_path
-    runner(cmd)
+    runner("git clone %s %s" % (source_url, destination_repo_path))
 
 
 def pull(source_repo_url, destination_repo_path, runner=None):
     """
-    Pull from the source repo into the repository at destination_repo_path.
+    Pull from the git repo at ``source_repo_url`` into the repository at
+    ``destination_repo_path``.
     """
     if runner is None:
         runner = run
     with cd(destination_repo_path):
         runner("git pull %s" % source_repo_url)
     
+def repo(source_repo, destination_path, runner=None):
+    """
+    Ensure that an up-to-date git repo is in place at ``destination_path``,
+    cloning or pulling from ``source_repo`` as necessary.
+    """
+    if runner is None:
+        runner = run
+    if is_git_repo(destination_path):
+        pull(source_repo, destination_path)
+    else:
+        clone(source_repo, destination_path)
