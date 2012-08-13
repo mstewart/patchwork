f315357b0926ec797d40b8a49c03b68f1302be14
Mike Stewart <mike@mikestewart.net>
2012-08-13 14:53:22 -0700

    Actual mysql tests.

--- test/test_mysql.py
+++ test/test_mysql.py
@@ -5,8 +5,15 @@ from mock import patch
 
 class MysqlInstallationTest(TestCase):
     @patch('patchwork.packages.distro_family')
-    @patch('patchwork.packages.rpm')
-    def test_redhat_delegation(self, rpm, distro_family):
+    @patch('patchwork.packages.install')
+    def test_mysql_client_redhat_install(self, install, distro_family):
         distro_family.return_value = 'redhat'
-        packages.install('mypkg')
-        rpm.install.assert_called_once_with('mypkg')
+        mysql.client()
+        install.assert_called_once_with('mysql')
+
+    @patch('patchwork.packages.distro_family')
+    @patch('patchwork.packages.install')
+    def test_mysql_server_debian_install(self, install, distro_family):
+        distro_family.return_value = 'debian'
+        mysql.server()
+        install.assert_called_once_with('mysql-server')
